9f61f7cde5c80fe0b0a93faa278a0cc4b2d37d2f
h.yoshino <osushicrusher@gmail.com>
2022-02-05 11:39:15 +0900

    a

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pages from 'vite-plugin-pages'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), pages()]
 })
